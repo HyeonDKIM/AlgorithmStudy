cf428a753ae0a4afe03a5752d24c2c2ddce7fb4f
HyeonDKIM <45036629+HyeonDKIM@users.noreply.github.com>
2018-11-15 20:41:56 +0900

    Update 1977.cpp

--- 1977.cpp
+++ 1977.cpp
@@ -8,21 +8,17 @@ int main(void) {
 	cin >> M;
 	cin >> N;
 
-	int arr[100];
 	int Sum = 0;
 	int PerfectSquare = 0;
 	int minNum = 10000;
 
 	for (int i = 1; i*i <= N; i++) {
 		if (i*i >= M) {
-			arr[i] = i * i;
 			minNum = min(minNum, i*i);
 			Sum = Sum + (i*i);
 			PerfectSquare = 1;
 		}
 	}
-	sort(arr, arr + 100);
-
 	if (PerfectSquare == 0)
 		cout << -1;
 	if (PerfectSquare == 1) {
