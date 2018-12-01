948a857b128fea7f238bf48760ac0c479ab4bd38
HyeonDKIM <45036629+HyeonDKIM@users.noreply.github.com>
2018-12-01 15:44:46 +0900

    Create 10797.cpp

---
+++ 10797.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int date;
+  int car[5];
+  int count = 0;
+  cin >> date;
+  for (int i = 0; i < 5; i++)
+    cin >> car[i];
+  for (int i = 0; i < 5; i++) {
+    if (car[i] == date)
+      count++;
+  }
+  cout << count << endl;
+}
