90944c35ca0ab4dcf2f8aaa6d2630d7a56530dca
hyeonji kim <45036629+HyeonDKIM@users.noreply.github.com>
2020-07-10 05:05:30 +0900

    Update 1065.cpp

--- BOJ/1065.cpp
+++ BOJ/1065.cpp
@@ -1,23 +1,18 @@
+//#pragma warning (disable:4996)
 #include <iostream>
 using namespace std;
-
 //1065번 한수
-
-int HanArr[1000];
 
 int HanNumber(int N) {
 	int hun, ten, one;
 	if (N < 100)
-		return N;
-	else if (N < 1000) {
-		hun = N / 100;
-		ten = N % 100 / 10;
-		one = N % 10;
-		if ((2 * ten) == hun + one)
-			return N;
-	}
-	else
-		return 0;
+		return true;
+	hun = N / 100;
+	ten = N % 100 / 10;
+	one = N % 10;
+	if ((2*ten) == hun + one)
+		return true;
+	return false;
 }
 
 int main() {
@@ -26,12 +21,8 @@ int main() {
 	int count = 0;
 
 	for (int i = 1; i <= N; i++) {
-		HanArr[HanNumber(i)] = 1;
-	}
-	for (int i = 1; i <= N; i++) {
-		if (HanArr[i] == 1) {
+		if (HanNumber(i))
 			count++;
-		}
 	}
-	cout << count << endl;
+	cout << count;
 }
